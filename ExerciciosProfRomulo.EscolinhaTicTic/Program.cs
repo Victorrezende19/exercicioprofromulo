ac55552c9870b9ed8be9b62c714c972606bbe341
Victorrezende19 <victor.pr99@hotmail.com>
2022-07-06 11:59:00 -0300

    ataulizando

---
+++ ExerciciosProfRomulo.EscolinhaTicTic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.EscolinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercício 4- Crie um programa para a escolinha Tic Tic
+            calcular 3 notas de um aluno.
+            Entrar com o nome do aluno e as três notas
+            Mostrar a soma das notas e a média.
+            */
+
+
+            double num1, num2, num3; 
+            double media;
+            string nome;
+            Console.WriteLine("digite as 3 notas: ");
+            num1 = double.Parse(Console.ReadLine());
+            num2 = double.Parse(Console.ReadLine());
+            num3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite o seu nome: ");
+            nome = (Console.ReadLine());
+            media = (num1 + num2 + num3) / 3;
+            Console.WriteLine($"Olá {nome} a media das suas notas é {media} suas notas foram {num1},{num2} e {num3}");
+            if (media <= 7)
+            {
+                Console.WriteLine($"O aluno {nome},esta reprovado!" );
+              }
+            else
+            {
+                Console.WriteLine($"O aluno {nome} esta aprovado com a nota {media}! Parabens!");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ExerciciosProfRomulo.MatematicaBasica/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.MatematicaBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Exercício 3- No projeto MatematicaBasica crie um 
+                Programa que calcule a soma de dois números inteiros.
+                Considere que o usuário vai digitar esses dois números.
+          */
+            //primeiro declara as variaveis
+            int num1;
+            int num2;
+            int soma;
+            //entrada de dados
+            Console.WriteLine("digite 2 numeros: ");
+            
+            num1 = int.Parse(Console.ReadLine());
+           // Console.WriteLine("digite o segundo numero: ");
+            num2 = int.Parse(Console.ReadLine());
+            soma = num1 + num2;
+            Console.WriteLine(soma);
+        }
+    }
+}
